15d13a0b7d8e71346119c77321e3ec902e4fcf96
clara <10088152+claraj@users.noreply.github.com>
2022-09-01 10:38:20 -0500

    quick and untidy script to count known tags in a web page

---
+++ parse.py
@@ -0,0 +1,62 @@
+import requests 
+from bs4 import BeautifulSoup
+from pie import draw_pie
+
+# For downloading...
+url = 'https://www.minneapolis.edu' # todo fill in here
+page = requests.get(url).text
+
+# for reading from a file...
+# page = open('page.html', 'r').read()
+
+
+soup = BeautifulSoup(page, 'html.parser')
+
+all_tags = soup.find_all(True)
+
+names = [ tag.name for tag in all_tags ]
+
+tag_counts = {}
+
+for name in names:
+    if name in tag_counts:
+        tag_counts[name] = tag_counts[name] + 1
+    else:
+        tag_counts[name] = 1
+
+# print(tag_counts)
+
+draw_pie(tag_counts)
+
+# Tags covered in class in the first week
+
+week_1_tags = ['p', 'a', 'b', 'img', 'div', 'span', 'i', 
+'table', 'td', 'tr', 'th', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
+'link', 'html', 'body', 'title', 'head', 'li', 'ul', 'ol', 'br', 'strong']
+
+week_2_tags = ['option', 'select', 'button', 'input', 'form']
+
+known_vs_unknown = {
+    'known': 0,
+    'unknown': 0
+}
+
+unknown_set = set()
+
+for name in names:
+    if name in week_1_tags:
+        known_vs_unknown['known'] += 1
+    else:
+        unknown_set.add(name)
+        known_vs_unknown['unknown'] += 1
+
+print(f'unknown tags: {unknown_set}')
+print(known_vs_unknown)
+
+if known_vs_unknown['known'] + known_vs_unknown['unknown'] == 0:
+    # no tags!
+    print('There are no tags.')
+else:
+    percentage_known = known_vs_unknown['known'] / ( known_vs_unknown['known'] + known_vs_unknown['unknown'] )  * 100
+    draw_pie(known_vs_unknown)
+    print(f'This page has {percentage_known:.1f}% known tags.')
---
+++ pie.py
@@ -0,0 +1,38 @@
+"""
+https://docs.bokeh.org/en/latest/docs/gallery/pie_chart.html
+"""
+
+from math import pi
+
+import pandas as pd
+
+from bokeh.palettes import Category20
+from bokeh.plotting import figure, show
+from bokeh.transform import cumsum
+
+def draw_pie(x):
+
+    data = pd.Series(x).reset_index(name='value').rename(columns={'index': 'element'})
+    data['angle'] = data['value']/data['value'].sum() * 2*pi
+
+    data_point_count = len(x)
+    biggest_pallete = Category20[len(Category20) - 1]
+    pallete_len = len(biggest_pallete)
+    repeats = (int( data_point_count / pallete_len )) + 1
+    pallete = biggest_pallete * repeats
+    pallete = pallete[:len(x)]  # slice len of data 
+
+    data['color'] = pallete#['#23f423'] * len(x) # A list of color values, one per data point
+
+    p = figure(height=1000, title="Pie Chart", toolbar_location=None,
+            tools="hover", tooltips="@element: @value", x_range=(-0.5, 1.0))
+
+    p.wedge(x=0, y=1, radius=0.4,
+            start_angle=cumsum('angle', include_zero=True), end_angle=cumsum('angle'),
+            line_color="white", fill_color='color', legend_field='element', source=data)
+
+    p.axis.axis_label = None
+    p.axis.visible = False
+    p.grid.grid_line_color = None
+
+    show(p)
